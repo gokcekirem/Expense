6fea533e2e7b3011621ff96abc6514300d5fb351
Irem Gokcek <44651881+iremgokcek@users.noreply.github.com>
2018-12-05 23:53:11 +0300

    changed route configuration

--- ExpenseProject/App_Start/RouteConfig.cs
+++ ExpenseProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ExpenseProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Expenses", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
