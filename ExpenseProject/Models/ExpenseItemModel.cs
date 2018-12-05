1bb26bf0eaa390bf76a47551e0920bae4f6bdeec
Irem Gokcek <44651881+iremgokcek@users.noreply.github.com>
2018-12-05 23:03:33 +0300

    Model

---
+++ ExpenseProject/Models/ExpenseItemModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data;
+
+namespace ExpenseProject.Models
+{
+    public class ExpenseItemModel
+    {
+
+        public IEnumerable<Data.ExpenseItem> ExpenseItem;
+        public decimal TotalSum;
+        public int ExpenseID;
+
+    }
+    public enum ExpenseStatus
+    {
+        Draft = 0,
+        ManagerApprovalPending = 1,
+        AccountingPaymentPending = 2,
+        ReturnedForCorrection = 3,
+        Deleted = 4,
+        Paid = 5
+    }
+}
